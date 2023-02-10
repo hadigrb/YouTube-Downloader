129714af627d114ec23b09eabe552107511b59d7
hadigorbanii <mr.hadigrb@gmail.com>
2023-02-10 21:31:35 +0330

    YouTube Downloader

--- YouTubeDownloader/main/views.py
+++ YouTubeDownloader/main/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.views.generic import View
+from pytube import *
+import sys
 
-# Create your views here.
+class home(View):
+    def __init__(self,url=None):
+        self.url = url
+    def get(self,request):
+        return render(request,'main/home.html')
+    def post(self,request):
+        if request.POST.get('fetch-vid'):
+            self.url = request.POST.get('given_url')
+            video = YouTube(self.url)
+            video_title , video_thumbnail = video.title , video.thumbnail_url
+            qual , stream = [] , []
+            for vid in video.streams.filter(progressive=True):
+                qual.append(vid.resolution)
+                stream.append(vid)
+            context = {
+                'video_title':video_title,
+                'video_thumbnail':video_thumbnail,
+                'qual':qual,
+                'stream':stream,
+                'url':self.url
+            }
+            return render(request,'main/home.html',context=context)
+        
+        elif request.POST.get('download-vid'):
+            self.url = request.POST.get('given_url')
+            video = YouTube(self.url)
+            stream = [x for x in video.streams.filter(progressive=True)]
+            choosen_qual = video.streams[int(request.POST.get('download-vid')) - 1]
+            choosen_qual.download(output_path='../../Downloads')
+            sys.exit()
+            return redirect('home')
+        return render(request,'main/home.html')
